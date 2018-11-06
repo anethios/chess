4a0f4dd3d9ba78fb3afaef84d4a474d1ad7afda3
Britton Bercegeay <britton.bercegeay@selu.edu>
2018-11-05 22:55:00 -0600

    Organization And Comments

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -51,13 +51,11 @@
         Application.Children.Remove(titleBG)
         Application.Children.Remove(titleButton)
 
-
         Dim x, y
         Dim z = 0
         Dim wButtons As Button() = {wR, wKn, wB, wK, wQ, wB2, wKn2, wR2, wp1B, wp2B, wp3B, wp4B, wp5B, wp6B, wp7B, wp8B}
         Dim bButtons As Button() = {bR, bKn, bB, bQ, bK, bB2, bKn2, bR2, bp1B, bp2B, bp3B, bp4B, bp5B, bp6B, bp7B, bp8B}
         Dim empties As Button() = {empty0, empty1, empty2, empty3, empty4, empty5, empty6, empty7, empty8, empty9, empty10, empty11, empty12, empty13, empty14, empty15, empty16, empty17, empty18, empty19, empty20, empty21, empty22, empty23, empty24, empty25, empty26, empty27, empty28, empty29, empty30, empty31}
-
 
         For x = 2 To 5
             For y = 0 To 7
@@ -67,7 +65,6 @@
                 z = z + 1
             Next
         Next
-
         For x = 0 To 7
             For y = 0 To 7
                 pieceSet(x, y).Captured = False
@@ -108,7 +105,6 @@
                 gameBoard(x, y) = pieceSet(x, y)
             Next
         Next
-
         For x = 0 To 1
             For y = 0 To 7
                 gameBoard(x, y).Button.IsEnabled = False
@@ -116,13 +112,10 @@
         Next
     End Sub
 
-
     '------------------------------------------------------------------------
     '                           Game Functions
     '------------------------------------------------------------------------
-
     Public Sub UpdateGame()
-
         If gameObject.turn = Turn.White Then
             turnDisplay.Text = "Turn: Black"
             gameObject.turn = Turn.Black
@@ -148,9 +141,7 @@
                 Next
             Next
         End If
-
     End Sub
-
 
     Public Function GetButtonsW()
         Dim wButtons As Button() = {wR, wKn, wB, wK, wQ, wB2, wKn2, wR2, wp1B, wp2B, wp3B, wp4B, wp5B, wp6B, wp7B, wp8B}
@@ -162,15 +153,13 @@
         Return bButtons
     End Function
 
-
-
     '------------------------------------------------------------------------
     '                            Game Initializer
     '------------------------------------------------------------------------
 
     Public Sub NewGame()
         gameObject.turn = Turn.White
-        
+
         For x = 0 To 7
             For y = 0 To 7
                 gameBoard(x, y) = pieceSet(x, y)
@@ -180,22 +169,8 @@
                     Grid.SetRow(pieceSet(x, y).Button, x)
                 End If
             Next
-
         Next
-
-
-        '---------------------------------------------------------------------
     End Sub
-
-    '------------------------------------------------------------------------
-    '                              Game Logic
-    '------------------------------------------------------------------------
-
-
-
-
-    '------------------------------------------------------------------------
-
 
     '------------------------------------------------------------------------
     '                              Piece Logic
@@ -217,8 +192,6 @@
         Dim refRow As Integer
         Dim refCol As Integer
 
-        'If the button that was clicked is equal to the first button of the white piece set then do...
-
         For x = 0 To blankes.Length - 1
             If blankes(x) IsNot Nothing Then
                 blankes(x).Visibility = Visibility.Hidden
@@ -227,13 +200,10 @@
 
         For x = 0 To 7
             For y = 0 To 7
-
                 If ReferenceEquals(sender, gameBoard(x, y).Button) Then
                     selected = gameBoard(x, y)
                     refRow = Grid.GetRow(sender)
                     refCol = Grid.GetColumn(sender)
-                    'Debug.Write(chessGrid.Children.IndexOf(sender))
-
                 End If
             Next
         Next
@@ -241,26 +211,40 @@
         If (selected.Team = Turn.White) Then
             Signature = -1
         End If
+        '---------------------------------------------KING-----------------------------------------
+        If (selected.Type = Type.King) Then
 
+        End If
+        '--------------------------------------------QUEEN-----------------------------------------
+        If (selected.Type = Type.Queen) Then
+
+        End If
+        '--------------------------------------------BISHOP----------------------------------------
+        If (selected.Type = Type.Bishop) Then
+
+        End If
+        '--------------------------------------------KNIGHT----------------------------------------
+        If (selected.Type = Type.Knight) Then
+
+        End If
+        '---------------------------------------------ROOK-----------------------------------------
         If (selected.Type = Type.Rook) Then
+            '----------------White: Up - Black: Down------------------
             While ((refRow + (c1 * Signature) <> 8) And (refRow + (c1 * Signature) <> -1))
                 If (gameBoard(refRow + (c1 * Signature), refCol).Type = Type.Empty Or gameBoard(refRow + (c1 * Signature), refCol).Team <> selected.Team) Then
                     blankes(count).Visibility = Visibility.Visible
                     Grid.SetRow(blankes(count), refRow + (c1 * Signature))
                     Grid.SetColumn(blankes(count), refCol)
                     count += 1
-
                     If gameBoard(refRow + (c1 * Signature), refCol).Type <> Type.Empty And gameBoard(refRow + (c1 * Signature), refCol).Team <> selected.Team Then
                         Exit While
                     End If
-
                     c1 += 1
-
                 Else
                     Exit While
                 End If
             End While
-
+            '----------------White: Right - Black: Left------------------
             While ((refCol + (c3 * Signature) <> 8) And (refCol + (c3 * Signature) <> -1))
                 If (gameBoard(refRow, (c3 * Signature) + refCol).Type = Type.Empty Or gameBoard(refRow, (c3 * Signature) + refCol).Team <> selected.Team) Then
                     blankes(count).Visibility = Visibility.Visible
@@ -276,7 +260,7 @@
                     Exit While
                 End If
             End While
-
+            '----------------White: Down - Black: Up------------------
             While ((refRow - (c2 * Signature) <> 8) And (refRow - (c2 * Signature) <> -1))
                 If (gameBoard(refRow - (c2 * Signature), refCol).Type = Type.Empty Or gameBoard(refRow - (c2 * Signature), refCol).Team <> selected.Team) Then
                     blankes(count).Visibility = Visibility.Visible
@@ -291,14 +275,13 @@
                     Exit While
                 End If
             End While
-
+            '----------------White: Left - Black: Right------------------
             While ((refCol - (c4 * Signature) <> 8) And (refCol - (c4 * Signature) <> -1))
                 If (gameBoard(refRow, refCol - (c4 * Signature)).Type = Type.Empty Or gameBoard(refRow, refCol - (c4 * Signature)).Team <> selected.Team) Then
                     blankes(count).Visibility = Visibility.Visible
                     Grid.SetRow(blankes(count), refRow)
                     Grid.SetColumn(blankes(count), refCol - (c4 * Signature))
                     count += 1
-
                     If gameBoard(refRow, refCol - (c4 * Signature)).Type <> Type.Empty And gameBoard(refRow, refCol - (c4 * Signature)).Team <> selected.Team Then
                         Exit While
                     End If
@@ -308,7 +291,7 @@
                 End If
             End While
         End If
-        '---------------------------------------PAWN---------------------------------------------
+        '---------------------------------------------PAWN-----------------------------------------
         If (selected.Type = Type.Pawn) Then
             If gameBoard(refRow + Signature, refCol).Type = Type.Empty Then
                 blankes(count).Visibility = Visibility.Visible
@@ -345,10 +328,6 @@
             End If
 
         End If
-
-
-
-
     End Sub
 
     Public Sub Move(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
@@ -379,20 +358,15 @@
                 If (gameBoard(x, y).Type = Type.Empty) And (gameBoard(x, y).Button.IsVisible = True) Then
                     gameBoard(x, y).Button.Visibility = Visibility.Collapsed
                 End If
-
                 If selected.Button Is gameBoard(x, y).Button Then
                     temp = gameBoard(x, y)
                     gameBoard(x, y) = gameBoard(row, col)
 
                     gameBoard(row, col) = temp
                     gameBoard(row, col).hasMoved = True
-
                 End If
             Next
         Next
-
         UpdateGame()
-
     End Sub
-
 End Class
