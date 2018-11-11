f81fccb0d8abcccd93a64abd593cf03f08eacccb
Britton Bercegeay <britton.bercegeay@selu.edu>
2018-11-11 15:39:59 -0600

    Attempted Checking By Tracing Paths

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -3,6 +3,7 @@
     Dim turnDisp As TextBox = turnDisplay
     Dim selected As Piece
     Dim previous As Piece
+    Dim fill As Button()
 
     Dim Signature = 1
     Dim Signature2 = 1
@@ -40,7 +41,7 @@
         Public Column As Integer
         Public Row As Integer
         Public Selected As Boolean
-        Public Captured As Boolean
+        Public Threat As Boolean
         Public Type As Type
         Public hasMoved As Boolean
     End Structure
@@ -48,6 +49,11 @@
     Dim gameObject As Game
     Dim pieceSet(7, 7) As Piece
     Dim gameBoard(7, 7) As Piece
+
+    Private Function getTrace()
+        Dim checkPath As Button() = {x0, x1, x2, x3, x4, x5, x6, x7}
+        Return checkPath
+    End Function
 
     Private Function getAvailableMoves()
         Dim blankes As Button() = {blank0, blank1, blank2, blank3, blank4, blank5, blank6, blank7, blank8, blank9, blank10, blank11, blank12, blank13, blank14, blank15, blank16, blank17}
@@ -83,7 +89,7 @@
         Next
         For x = 0 To 7
             For y = 0 To 7
-                pieceSet(x, y).Captured = False
+                pieceSet(x, y).Threat = False
                 pieceSet(x, y).Selected = False
                 pieceSet(x, y).Row = x
                 pieceSet(x, y).Column = y
@@ -180,9 +186,7 @@
                 Next
             Next
         End If
-
         CheckKing()
-
         If gameObject.checkBlack = True Then
             blackCheck.IsChecked = True
         Else
@@ -195,10 +199,61 @@
         End If
     End Sub
 
+    Public Sub TracePath(start As Button, finish As Button, signature As Integer)
+        Dim checkPath = getTrace()
+        Dim startRow, startCol, finishRow, finishCol, counter
+        counter = 0
+        startRow = Grid.GetRow(start)
+        startCol = Grid.GetColumn(start)
+        finishRow = Grid.GetRow(finish)
+        finishCol = Grid.GetColumn(finish)
+
+        If Grid.GetRow(start) = Grid.GetRow(finish) Then
+            While startCol <> finishCol - signature
+                Grid.SetRow(checkPath(counter), startRow)
+                Grid.SetColumn(checkPath(counter), startCol)
+                startCol += signature
+                counter += 1
+            End While
+        ElseIf Grid.GetColumn(start) = Grid.GetColumn(finish) Then
+            While startRow <> finishRow - signature
+                Grid.SetColumn(checkPath(counter), startCol)
+                Grid.SetRow(checkPath(counter), startRow)
+                startRow += signature
+                counter += 1
+            End While
+        Else
+            While startCol <> finishCol - signature And startRow <> finishRow - signature
+                Grid.SetColumn(checkPath(counter), startCol)
+                Grid.SetRow(checkPath(counter), startRow)
+                startCol += signature
+                startRow += signature
+                counter += 1
+            End While
+        End If
+    End Sub
+
+    Public Sub CompareTrace(ByRef blank As Button)
+        Dim checkPath = getTrace()
+        Dim x, y, z, a, b
+        x = 0
+        While x < checkPath.Length
+            y = Grid.GetRow(checkPath(x))
+            z = Grid.GetColumn(checkPath(x))
+            a = Grid.GetRow(blank)
+            b = Grid.GetColumn(blank)
+
+            If y <> a And z <> b Then
+                blank.Visibility = Visibility.Collapsed
+            End If
+            x += 1
+        End While
+    End Sub
+
     Public Sub CheckKing()
         Signature = 1
-        Dim l, z
-        Dim king As Piece
+        Dim l, z, counter
+        Dim king As Piece = Nothing
 
         Dim refRow = 0
         Dim refCol = 0
@@ -211,12 +266,10 @@
                 For y = 0 To 7
                     If gameBoard(x, y).Type = Type.King Then
                         If gameBoard(x, y).Team = Turn.White And Signature = -1 Then
-                            Debug.Write(gameBoard(x, y).Button.Name)
                             king = gameBoard(x, y)
                             refRow = x
                             refCol = y
                         ElseIf gameBoard(x, y).Team = Turn.Black And Signature = 1 Then
-                            Debug.Write(gameBoard(x, y).Button.Name)
                             king = gameBoard(x, y)
                             refRow = x
                             refCol = y
@@ -224,18 +277,98 @@
                     End If
                 Next
             Next
+            '-------------- Diagonals ----------------
             For x = 0 To 1
                 For y = 0 To 1
                     While (refRow + (value * Signature * Signature2) < 8) And (refRow + (value * Signature * Signature2) > -1) And (refCol + (value * Signature * Signature3) < 8) And (refCol + (value * Signature * Signature3) > -1)
                         If (gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type = Type.Empty Or gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Team <> king.Team) Then
 
-                            count += 1
+
                             If gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type <> Type.Empty And gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Team <> king.Team Then
                                 If gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type = Type.Bishop Or gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type = Type.Queen Then
+                                    'TracePath(king.Button, gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Button, Signature)
+                                    z = 1
+                                End If
+                                If value = 1 And gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type = Type.Pawn Then
+                                    'TracePath(king.Button, gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Button, Signature)
+                                    z = 1
+                                End If
+                                Exit While
+                            End If
+                            value += 1
+                        Else
+                            Exit While
+                        End If
+                    End While
+                    value = 1
+                    If (Signature2 = 1) Then
+                        Signature2 = -1
+                    Else
+                        Signature2 = 1
+                    End If
+                Next
+                Signature3 = -1
+            Next
 
-                                    Debug.Write("huh ")
-                                    Debug.Write(refRow + (value * Signature * Signature2))
-                                    Debug.Write(refCol + (value * Signature * Signature3))
+            '------------- Perpendiculars --------------
+            Signature4 = 0
+            For x = 0 To 1
+                For y = 0 To 1
+                    While ((refRow + (value * Signature * Signature2 * Signature3) < 8) And (refRow + (value * Signature * Signature2 * Signature3) > -1) And (refCol + (value * Signature * Signature2 * Signature4) < 8) And (refCol + (value * Signature * Signature2 * Signature4) > -1))
+                        If (gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type = Type.Empty Or gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Team <> king.Team) Then
+
+                            If gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type <> Type.Empty And gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Team <> king.Team Then
+                                If gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type = Type.Rook Or gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type = Type.Queen Then
+                                    'TracePath(king.Button, gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Button, Signature)
+                                    z = 1
+                                End If
+                                Exit While
+                            End If
+                            value += 1
+                        Else
+                            Exit While
+                        End If
+                    End While
+                    count += 1
+                    value = 1
+                    If Signature2 = 1 Then
+                        Signature2 = -1
+                    Else
+                        Signature2 = 1
+                    End If
+                Next
+                value = 1
+                Signature4 = 1
+                Signature3 = 0
+            Next
+
+            If z = 1 Then
+                CheckSignatures(Signature, 1)
+            Else
+                CheckSignatures(Signature, 0)
+            End If
+            Signature = -1
+        Next
+    End Sub
+
+    Public Sub CheckPrevention(ByRef blank As Button)
+        Signature = 1
+        Dim l, z
+
+        Dim refRow = Grid.GetRow(blank)
+        Dim refCol = Grid.GetColumn(blank)
+
+        For l = 0 To 1
+            Signature2 = 1
+            Signature3 = 1
+            z = 0
+            '-------------- Diagonals ----------------
+            For x = 0 To 1
+                For y = 0 To 1
+                    While (refRow + (value * Signature * Signature2) < 8) And (refRow + (value * Signature * Signature2) > -1) And (refCol + (value * Signature * Signature3) < 8) And (refCol + (value * Signature * Signature3) > -1)
+                        If (gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type = Type.Empty Or gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Team <> gameObject.turn) Then
+                            If gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type <> Type.Empty And gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Team <> gameObject.turn Then
+                                If gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type = Type.Bishop Or gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type = Type.Queen Then
                                     z = 1
                                 End If
                                 If value = 1 And gameBoard(refRow + (value * Signature * Signature2), refCol + (value * Signature * Signature3)).Type = Type.Pawn Then
@@ -258,15 +391,15 @@
                 Signature3 = -1
             Next
 
+            '------------- Perpendiculars --------------
             Signature4 = 0
             For x = 0 To 1
                 For y = 0 To 1
                     While ((refRow + (value * Signature * Signature2 * Signature3) < 8) And (refRow + (value * Signature * Signature2 * Signature3) > -1) And (refCol + (value * Signature * Signature2 * Signature4) < 8) And (refCol + (value * Signature * Signature2 * Signature4) > -1))
-                        If (gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type = Type.Empty Or gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Team <> king.Team) Then
+                        If (gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type = Type.Empty Or gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Team <> gameObject.turn) Then
 
-                            If gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type <> Type.Empty And gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Team <> king.Team Then
+                            If gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type <> Type.Empty And gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Team <> gameObject.turn Then
                                 If gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type = Type.Rook Or gameBoard(refRow + (value * Signature * Signature2 * Signature3), refCol + (value * Signature * Signature2 * Signature4)).Type = Type.Queen Then
-
                                     z = 1
                                 End If
                                 Exit While
@@ -287,13 +420,9 @@
                 Signature4 = 1
                 Signature3 = 0
             Next
-
             If z = 1 Then
-                CheckSignatures(Signature, 1)
-            Else
-                CheckSignatures(Signature, 0)
+                blank.Visibility = Visibility.Collapsed
             End If
-
             Signature = -1
         Next
     End Sub
@@ -399,6 +528,7 @@
                             blankes(count).Visibility = Visibility.Visible
                             Grid.SetRow(blankes(count), refRow + (value * Signature * Signature2))
                             Grid.SetColumn(blankes(count), refCol + (value * Signature * Signature3))
+                            CheckPrevention(blankes(count))
                             count += 1
                             value += 1
                         End If
@@ -421,6 +551,7 @@
                             blankes(count).Visibility = Visibility.Visible
                             Grid.SetRow(blankes(count), refRow + (value * Signature * Signature2 * Signature3))
                             Grid.SetColumn(blankes(count), refCol + (value * Signature * Signature2 * Signature4))
+                            CheckPrevention(blankes(count))
                             count += 1
                             value += 1
                         End If
@@ -578,10 +709,17 @@
                     nextCol = refCol + (value * Signature * Signature2 * Signature4)
                     While ((nextRow < 8) And (nextRow > -1) And (nextCol < 8) And (nextCol > -1))
                         If (gameBoard(nextRow, nextCol).Type = Type.Empty Or gameBoard(nextRow, nextCol).Team <> selected.Team) Then
-                            Debug.Write("WTF")
+
                             blankes(count).Visibility = Visibility.Visible
                             Grid.SetRow(blankes(count), nextRow)
                             Grid.SetColumn(blankes(count), nextCol)
+                            'If gameObject.checkWhite Or gameObject.checkBlack Then
+                            'If Signature = 1 Then
+                            'CompareTrace(blankes(count))
+                            'Else
+                            '   CompareTrace(blankes(count))
+                            'End If
+                            'End If
                             count += 1
                             value += 1
                         Else
@@ -640,6 +778,7 @@
                 End If
             End If
         End If
+
     End Sub
 
     Public Sub Move(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
